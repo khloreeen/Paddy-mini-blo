859aba6b5a49f9bbc84e765f73428291393cdbe8
Khloreeen <okubadejo2@gmail.com>
2024-09-03 20:33:47 +0100

    addes js file

---
+++ blog.js
@@ -0,0 +1,8 @@
+const formArea = document.getDocumentbyClass("form-area")
+const fullName = document.getElementById("fullname")
+const email = document.getElementById("email")
+const password = document.getElementById("password")
+
+formArea.addEventListener('submit', (e) => {
+
+})
